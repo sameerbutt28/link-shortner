1d2f2410760e394c2820dc77800e52d06998f6a4
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-11-22 07:13:47 +0000

    link shoertening code by sameer

--- src/App.jsx
+++ src/App.jsx
@@ -1,69 +1,45 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-function App() {
-    const [input, setInput] = useState("");
-    const [list, setList] = useState([]);
-    const [originalLink, setOriginalLink] = useState('');
-    const [shortenedLink, setShortenedLink] = useState('');
+const LinkShortener = () => {
+  const [originalLink, setOriginalLink] = useState('');
+  const [shortenedLink, setShortenedLink] = useState('');
 
-    const handleInputChange = (e) => {
-        setInput(e.target.value); //this function will take the value wrtitten in text bar and set as in setInput function 
-    };  
-  
-    const handleList = () => {
-        if (input) {  
-            setList([...list, input]);
-            setInput("");
-        }
+  const handleChange = (e) => {
+    setOriginalLink(e.target.value);
+  };
+
+  const shortenLink = async () => {
+    try {
+      const url = `https://cleanuri.com/api/v1/shorten?url=${encodeURIComponent(originalLink)}`;
+      
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const data = await response.json();
+        setShortenedLink(data.result_url);
+      } else {
+        console.error('Error shortening link:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error connecting to the server', error);
     }
+  };
 
-
-    return (
-        <div className="App">
-            <input type="text" placeholder="Paste your link" onChange={handleInputChange} value={input}/>
-            <button onClick={handleList}>
-                Click Here!
-            </button>
-            <ol>
-                {list.map((input, index) => (
-                    <li key={index} >
-
-                        {input}
-
-                    </li>
-                ))}
-            </ol>
-
+  return (
+    <div>
+      <label>
+        Paste your link:
+        <input type="text" value={originalLink} onChange={handleChange} />
+      </label>
+      <button onClick={shortenLink}>Shorten Link</button>
+      {shortenedLink && (
+        <div>
+          <p>Shortened Link:</p>
+          {shortenedLink}
         </div>
-    );
-}
+      )}
+    </div>
+  );
+};
 
-export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export default LinkShortener;
