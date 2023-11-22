4be7f8909f0cdecf7dcb5d918709ccba043efd29
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-11-22 07:31:46 +0000

    addimg comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,19 @@ const LinkShortener = () => {
   const [shortenedLink, setShortenedLink] = useState('');
 
   const handleChange = (e) => {
-    setOriginalLink(e.target.value);
+    setOriginalLink(e.target.value); //takes the value from the input bar and storing or setting it to the orgignal link using setOrignalLink.
   };
 
   const shortenLink = async () => {
     try {
-      const url = `https://cleanuri.com/api/v1/shorten?url=${encodeURIComponent(originalLink)}`;
+      const url = `https://cleanuri.com/api/v1/shorten?url=${encodeURIComponent(originalLink)}`; // we are using get method here and we are requesting the clean uri api and we are sending our long link and getiing the shortend link.
+      // here we are using encodedURIComponent so thaat the the url we are sending goes in correct form must not add some spaces it corrects the foormat of the url basically. 
       
-      const response = await fetch(url);
+      const response = await fetch(url); //fetchges the url  and storing in thew response 
 
       if (response.ok) {
         const data = await response.json();
-        setShortenedLink(data.result_url); 
+        setShortenedLink(data.result_url);  // if everything is good then get the result_url property .
       } else {  
         console.error('Error shortening link:', response.statusText);
       }
@@ -32,7 +33,7 @@ const LinkShortener = () => {
         <input type="text" value={originalLink} onChange={handleChange} />
       </label>
       <button onClick={shortenLink}>Shorten Link</button>
-      {shortenedLink && (
+      {shortenedLink && ( // if there is some  shortend link there and the string is not empty or null only then will be the lower div will run .
         <div>
           <p>Shortened Link:</p>
           {shortenedLink}
