d91f7f70e3352bfa7c701edaef9e4a0ba198bc17
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-11-28 15:19:24 +0000

    about to end code

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+// Parse JSON and URL-encoded bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Define a route
+app.get('/url-butt', async (req, res) => {
+  const url_cu = `https://cleanuri.com/api/v1/shorten?url=https://google.com`;
+  const response = await fetch(url_cu,{
+    method: 'POST',
+  })
+
+  const data = await response.json()
+  res.send(data)
+
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ frontend/src/App.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import axios from "axios";
+
+
+const LinkShortener = () => {
+  const [inputLink, setInputLink] = useState('');
+  const [shortenedLink, setShortenedLink] = useState('');
+
+  const handleChange = (e) => {
+    setInputLink(e.target.value); //takes the value from the input bar and storing or setting it to the orgignal link using setInputLink.
+  };
+
+  const shortenLink = async () => {
+    try {
+
+      const url_s = " http://localhost:3000"; 
+
+      const response = await fetch(url_s,
+        {
+          body:
+          {
+
+            "url": "http://google.com/"
+          }
+        })
+        ;
+
+      if (response.ok) {
+        const data = await response.json();
+        setShortenedLink(data.result_url);  // if everything is good then get the result_url property .
+      } else {
+        console.error('Error:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error', error);
+    }
+  }
+
+  return (
+    <div>
+      <label>
+        Paste your link:
+        <input type="text" value={inputLink} onChange={handleChange} />
+      </label>
+      <button onClick={shortenLink}>Shorten Link</button>
+      {shortenedLink && ( // if there is some  shortend link there and the string is not empty or null only then will be the lower div will run .
+        <div>
+          <p>Shortened Link:</p>
+          {shortenedLink}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LinkShortener;
