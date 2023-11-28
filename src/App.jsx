1ba58f011668e251c15d39f893113eae75df1de0
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-11-28 13:18:06 +0000

    umairs edited code but not yet working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import axios from "axios";
+
 
 const LinkShortener = () => {
   const [inputLink, setInputLink] = useState('');
@@ -10,26 +12,71 @@ const LinkShortener = () => {
 
   const shortenLink = async () => {
     try {
-      const url = `https://cleanuri.com/api/v1/shorten?url=${encodeURIComponent(inputLink)}`; // we are using get method here and we are requesting the clean uri api and we are sending our long link and getiing the shortend link.
-      // here we are using encodedURIComponent so thaat the the url we are sending goes in correct form must not add some spaces it corrects the foormat of the url basically. 
-      
-      const response = await fetch(url); //fetchges the url  and storing in thew response 
 
-      if (response.ok) {
-        const data = await response.json();
-        setShortenedLink(data.result_url);  // if everything is good then get the result_url property .
-      } else {  
-        console.error('Error:', response.statusText);
+      // const url_s = "https://cleanuri.com/api/v1/shorten"; // we are using get method here and we are requesting the clean uri api and we are sending our long link and getiing the shortend link.
+      // here we are using encodedURIComponent so thaat the the url we are sending goes in correct form must not add some spaces it corrects the foormat of the url basically. 
+
+
+      let headersList = {
+        "Accept": "*/*",
+        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
+        "Content-Type": "application/json"
       }
+
+      let bodyContent = JSON.stringify({
+        "url": "https%3A%2F%2Fgoogle.com%2F"
+      });
+
+      let response = await fetch("https://cleanuri.com/api/v1/shorten", {
+        method: "POST",
+        mode: "no-cors",
+        body: bodyContent,
+        headers: headersList
+      });
+
+      let data = await response.text();
+      console.log(data);
+
+
+
+
+
+      // const response = await fetch(url_s,
+      //   {
+      //     mode: 'no-cors',
+      //     method: "POST",
+      //     headers: {
+      //       'Content-Type': 'application/json',
+      //       'Access-Control-Allow-Origin': '*',
+      //     },
+      //     body: JSON.stringify(
+      //       {
+      //         "url": "http://google.com/"
+      //       }
+      // )})
+      // ;
+      //fetchges the url  and storing in thew response 
+
+
+
+
+
+
+      // if (response.ok) {
+      //   const data = await response.json();
+      //   setShortenedLink(data.result_url);  // if everything is good then get the result_url property .
+      // } else {  
+      //   console.error('Error:', response.statusText);
+      // }
     } catch (error) {
       console.error('Error', error);
     }
-  };
+  }
 
   return (
     <div>
       <label>
-        Paste your link:
+        Paste your link: 
         <input type="text" value={inputLink} onChange={handleChange} />
       </label>
       <button onClick={shortenLink}>Shorten Link</button>
