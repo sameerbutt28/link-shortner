f58e0e2ee4282550fa10697216231f9d63144fd7
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-11-28 19:24:33 +0000

    changed the hard coded value of the url in body to a suitable useState

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,38 @@
 import React, { useState } from 'react';
-import axios from "axios";
-
+import axios from 'axios';
 
 const LinkShortener = () => {
   const [inputLink, setInputLink] = useState('');
   const [shortenedLink, setShortenedLink] = useState('');
 
   const handleChange = (e) => {
-    setInputLink(e.target.value); //takes the value from the input bar and storing or setting it to the orgignal link using setInputLink.
+    setInputLink(e.target.value);
   };
 
   const shortenLink = async () => {
     try {
+      const url_s = 'http://localhost:3000';
 
-      const url_s = " http://localhost:3000"; 
-
-      const response = await fetch(url_s,
-        {
-          body:
-          {
-
-            "url": "http://google.com/"
-          }
-        })
-        ;    
+      const response = await fetch(url_s, {
+        method: 'POST', // Specify the HTTP method as POST
+        headers: {
+          'Content-Type': 'application/json', // Set the content type to JSON
+        },
+        body: JSON.stringify({
+          url: inputLink, // Use the inputLink state here
+        }),
+      });
 
       if (response.ok) {
         const data = await response.json();
-        setShortenedLink(data.result_url);  // if everything is good then get the result_url property .
+        setShortenedLink(data.result_url);
       } else {
         console.error('Error:', response.statusText);
       }
     } catch (error) {
       console.error('Error', error);
     }
-  }
+  };
 
   return (
     <div>
@@ -43,7 +41,7 @@ const LinkShortener = () => {
         <input type="text" value={inputLink} onChange={handleChange} />
       </label>
       <button onClick={shortenLink}>Shorten Link</button>
-      {shortenedLink && ( // if there is some  shortend link there and the string is not empty or null only then will be the lower div will run .
+      {shortenedLink && (
         <div>
           <p>Shortened Link:</p>
           {shortenedLink}
