07779cbeb85fe8e7010736c434dc273c8bebd9d6
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-12-01 17:54:15 +0000

    bs

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
     
-// Define a route     
+// Define a route       
 app.get('/url-butt', async (req, res) => {
   const url_cu = `https://cleanuri.com/api/v1/shorten?url=https://google.com`;
   const response = await fetch(url_cu,{
