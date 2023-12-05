5eba2b0a8a955eaa9ba2f62fd1aa042bc9ff19a7
Sameer Ali Butt <108467884+sameerbutt28@users.noreply.github.com>
2023-12-05 18:19:09 +0000

    bs

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();       
 const port = 3000;  
-
+  
 // Parse JSON and URL-encoded bodies        
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
